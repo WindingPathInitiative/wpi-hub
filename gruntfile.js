6b884a81a461a2e3ce841ba820ac87fd002a8cf4
Ephraim Gregor <ephraim@ephraimgregor.com>
2015-09-13 23:26:44 -0400

    Updates templates, adds grunt parsing.

---
+++ gruntfile.js
@@ -0,0 +1,66 @@
+/**
+ * Grunt dev helper and validator.
+ * @author Ephraim Gregor
+ */
+
+module.exports = grunt => {
+	'use strict';
+
+	grunt.initConfig({
+		watch: {
+			options: {
+				spawn: false,
+				livereload: true
+			},
+			js: {
+				files: '**/*.js',
+				tasks: [ 'jshint', 'jscs' ]
+			},
+            css: {
+                files: 'public/stylesheets/*.css',
+                tasks: [ 'csslint' ]
+            }
+		},
+
+		jshint: {
+			app: {
+                options: {
+                    esnext: true
+                },
+                files: [ '*.js', 'routes/*.js' ]
+            },
+            frontend: {
+                options: {
+                    browser: true
+                },
+                files: [ 'public/javascripts/*.js' ]
+            },
+			options: {
+				strict: true,
+                reporter: require( 'jshint-stylish' )
+			},
+		},
+		jscs: {
+			all: [ '**/*js' ],
+		},
+
+		csslint: {
+			all: {
+				options: {
+					import: 2,
+					csslintrc: '.csslintrc'
+				},
+				src: [ 'public/stylesheets/*.css' ],
+			},
+		},
+	});
+
+	// Sets watch to prevent exit.
+	grunt.event.on( 'watch', function() {
+        grunt.option( 'force', true );
+    });
+
+	require( 'load-grunt-tasks' )( grunt );
+
+	grunt.registerTask( 'default', [ 'watch' ] );
+};

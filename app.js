67626e7954eec0871dae55a96de5750b7eeff061
Ephraim Gregor <ephraim@ephraimgregor.com>
2015-09-13 23:55:08 -0400

    Fix stylus middleware, use nib.

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
 'use strict';
 
-var express = require('express'),
-    path = require('path'),
-    favicon = require('serve-favicon'),
-    logger = require('morgan'),
+var express      = require('express'),
+    path         = require('path'),
+    favicon      = require('serve-favicon'),
+    logger       = require('morgan'),
     cookieParser = require('cookie-parser'),
-    bodyParser = require('body-parser'),
+    bodyParser   = require('body-parser'),
+    stylus       = require( 'stylus' ),
 
-    routes = require('./routes/index'),
-    users = require('./routes/users'),
+    routes       = require('./routes/index'),
+    users        = require('./routes/users'),
 
-    app = express();
+    app          = express();
 
-// view engine setup
+// View engine setup.
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// Middleware.
 app.use(favicon(path.join(__dirname, 'public/images', 'favicon.png')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use( stylus.middleware({
+    src: path.join( __dirname, 'public' ),
+    compile: ( str, path ) => {
+        return stylus( str )
+            .set( 'filename', path )
+            .use( require( 'nib' )() )
+            .import( 'nib' );
+    }
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);

2fcbb67e6c0ed9c6512a0c95d1120526b8c59d92
Ephraim Gregor <ephraim@ephraimgregor.com>
2015-09-17 22:06:47 -0400

    Breaks out navigation module.

--- common/index.js
+++ common/index.js
@@ -1,25 +1,9 @@
 'use strict';
 
-var nav      = require( './config/nav.json' ),
-    _        = require( 'lodash' ),
-    rootPath = process.env.domain || 'http://portal.mindseyesociety.org',
-    navMap;
-
-// Run nav object through a map to prepend root.
-navMap = i => {
-    if ( i.url && i.url.indexOf( '/' ) === 0 ) {
-        i.url = rootPath + i.url;
-    }
-    if ( i.children ) {
-        i.children.map( navMap );
-    }
-    return i;
-};
-
-nav.map( navMap );
-
 module.exports = ( req, res, next ) => {
-    res.locals.nav      = nav;
-    res.locals.rootPath = rootPath;
+
+    // Navigation.
+    require( './nav' )( res );
+
     next();
 };
---
+++ common/nav.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * Module to set up navigation for system.
+ */
+
+var nav      = require( './config/nav.json' ),
+    _        = require( 'lodash' ),
+    rootPath = process.env.domain || 'http://portal.mindseyesociety.org',
+    navMap;
+
+// Run nav object through a map to prepend root.
+navMap = i => {
+    if ( i.url && i.url.indexOf( '/' ) === 0 ) {
+        i.url = rootPath + i.url;
+    }
+    if ( i.children ) {
+        i.children.map( navMap );
+    }
+    return i;
+};
+
+nav.map( navMap );
+
+module.exports = ( res ) => {
+    res.locals.nav      = nav;
+    res.locals.rootPath = rootPath;
+};

3698efa6df45802c6ca070f14500ef05dd8415ea
Ephraim Gregor <ephraim@ephraimgregor.com>
2016-04-04 01:14:55 -0400

    Adds network checking to permissions endpoint.

--- controllers/permissions.js
+++ controllers/permissions.js
@@ -4,14 +4,16 @@
  * User data routes.
  */
 
-const router = require( 'express' ).Router();
-const token  = require( '../middlewares/token' );
-const Office = require( '../models/offices' );
+const router  = require( 'express' ).Router();
+const token   = require( '../middlewares/token' );
+const network = require( '../middlewares/network' );
+const Office  = require( '../models/offices' );
 
 /**
  * Gets the current user permissions.
  */
 router.get( '/',
+	network.internal,
 	token.validate(),
 	( req, res, next ) => {
 		new Office()

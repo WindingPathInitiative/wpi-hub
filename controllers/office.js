0a46790b1379d8712db3c875052c17437c6e0a9b
Ephraim Gregor <ephraim@ephraimgregor.com>
2016-12-25 20:12:22 -0500

    Adds endpoint to verify office permissions.

--- controllers/office.js
+++ controllers/office.js
@@ -360,4 +360,26 @@ router.get( '/verify/user/:user(\\d+)',
 );
 
 
+/**
+ * Verifies a user is an officer over a given user.
+ * Internal only.
+ */
+router.get( '/verify/office/:office(\\d+)',
+	network.internal,
+	token.parse(),
+	token.expired,
+	( req, res, next ) => {
+		if ( ! req.query.roles ) {
+			return next( new UserError( 'Missing required "roles" param', 400 ) );
+		}
+		let roles = req.query.roles.split( ',' );
+		perm.hasOverOffice( Number.parseInt( req.params.office ), roles, req.token.get( 'user' ) )
+		.then( () => {
+			res.json({ success: true });
+		})
+		.catch( err => next( new UserError( err.message, 403 ) ) );
+	}
+);
+
+
 module.exports = router;
